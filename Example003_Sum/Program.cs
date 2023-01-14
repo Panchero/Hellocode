5b11dd7ac8c3d4b2d33821b8ab2a08fca103d8ea
Семен <marbledrop@yandex.ru>
2023-01-14 23:22:48 +0300

    Add random

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA =  new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine (numberA); // Показать число
+int numberB = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine (numberB); // Показать число
+int result = numberA + numberB;
+Console.WriteLine(result);
